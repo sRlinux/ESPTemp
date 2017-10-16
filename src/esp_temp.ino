5092924b9f2e60842cfa31404ee430ce73e2f9ae
Sören Ritter <soeren@srage.de>
2017-10-16 21:47:43 +0200

    Update esp_temp.ino

--- src/esp_temp.ino
+++ src/esp_temp.ino
@@ -1,36 +1,43 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
 #define ONE_WIRE_BUS 2
 
-const char* ssid = "YOURSSID";
-const char* password = "YOURPASSWORD";
+#define mqtt_server "serverip"
+#define mqtt_user "user"
+#define mqtt_password "pass"
+#define temp0 "sensor0_temp0"
+#define temp1 "sensor0_temp1"
+
+const char* ssid = "SSID";
+const char* password = "PW"; 
 
 OneWire oneWire(ONE_WIRE_BUS);
 
 DallasTemperature sensors(&oneWire);
 
-WiFiClient serverClient;
-WiFiServer server(1234);
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setup(void)
 {
     WiFi.begin(ssid, password);
+    client.setServer(mqtt_server, 1883);
     sensors.begin();
-    server.begin();
+
+    sensors.requestTemperatures();
+
+    float temps0 = sensors.getTempCByIndex(0);
+    float temps1 = sensors.getTempCByIndex(1);
+
+    client.connect("sensor0", mqtt_user, mqtt_password);
+    client.publish(temp0, String(temps0).c_str() , true);
+    client.publish(temp1, String(temps1).c_str() , true);
+    ESP.deepSleep(600000000); // 10 minutes.
 }
 
 void loop(void)
 {
-    sensors.requestTemperatures();
-
-    float temp1 = sensors.getTempCByIndex(0);
-    float temp2 = sensors.getTempCByIndex(1);
-    WiFiClient client = server.available();
-
-    client.print(temp1, 2);
-    client.print(" ");
-    client.print(temp2, 2);
-    delay(1);
 }
